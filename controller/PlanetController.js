33838f84417645ca0fd4b442acac2b895c132e26
edimardaros <edimar.daros@gmail.com>
2022-04-05 22:58:26 -0300

    Transformando em uma API - aula 3

--- controller/PlanetController.js
+++ controller/PlanetController.js
@@ -11,5 +11,31 @@ module.exports = {
   async index(req, res) {
     const planets = await Planet.findAll();
     return res.json(planets);
-  }
+  },
+
+  async put(req,res) {
+    const {name, size, position} = req.body;
+    await Planet.update(
+        {name, size, position},
+        {
+          where: {
+            id : req.params.id,
+          }
+        }
+      );
+      return res.send('Planeta atualizado com sucesso!');
+  },
+
+  async delete(req,res) {
+    const {name, size, position} = req.body;
+    await Planet.destroy(
+        {
+          where: {
+            id : req.params.id,
+          }
+        }
+      );
+      return res.send('Planeta deletado com sucesso!');
+  },
+
 };
--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,7 @@ const PlanetController = require('../controller/PlanetController');
 
 routes.post('/planets', PlanetController.store);
 routes.get('/planets', PlanetController.index);
+routes.put('/planets/:id', PlanetController.put);
+routes.delete('/planets/:id', PlanetController.delete);
 
 module.exports = routes;
