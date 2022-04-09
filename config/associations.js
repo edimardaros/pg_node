aca5cea6f8b52b4442569e733e73a7e1d9601460
edimardaros <edimar.daros@gmail.com>
2022-04-08 21:07:38 -0300

    Associação Has Many

--- config/associations.js
+++ config/associations.js
@@ -1,7 +1,9 @@
 const Planet = require('../models/planet');
 const Satelite = require('../models/satelite');
 
-Planet.hasOne(Satelite, {onDelete: "CASCADE", onUpdate: "CASCADE"});
+// Planet.hasOne(Satelite, {onDelete: "CASCADE", onUpdate: "CASCADE"});
+Planet.hasMany(Satelite, {onDelete: "CASCADE", onUpdate: "CASCADE"});
 Satelite.belongsTo(Planet, {foreignKey:"planetId", as: "planet"});
 
 module.exports = {Planet, Satelite};
+
